9ea3ff03e691f30f84b5516e2e9be5856ca6ee8b
Pedro <pedropaulo3521@gmail.com>
2020-09-22 18:27:35 -0300

    Mais uma tentativa de build

--- src/components/Photo/PhotoComments/FormComment/index.js
+++ src/components/Photo/PhotoComments/FormComment/index.js
@@ -1,5 +1,5 @@
 import React, { useRef } from 'react';
-import useForm from '../../../../hooks/useForm';
+import useForm from '../../../../hooks/UseForm';
 import Input from '../../../Input';
 import { ReactComponent as EnviarIcon } from '../../../../assets/enviar.svg';
 import API from '../../../../API';
